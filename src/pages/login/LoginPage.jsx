f0cf776af9d749ef24b0ec07bdba823c68dc2cbd
orestis tzourmpakis <otzurbakis@hotmail.com>
2022-01-24 11:30:23 +0200

    commit

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,13 +1,18 @@
 import { Person, Lock, RotateLeftSharp } from "@mui/icons-material";
 import LoginCustomInput from "./LoginCustomInput";
 import "./LoginPage.css";
-import { login, checkIfExpired } from "../../services/userAccountService";
+import {
+  login,
+  checkIfExpired,
+  googleLogin,
+} from "../../services/userAccountService";
 import { useContext, useState } from "react";
 import { useAuth } from "../../contexts/userContext";
 import { roles } from "../../config.json";
 import { useNavigate } from "react-router-dom";
 import { handleErrors } from "../../utilities/handleErrors";
 import { UserContext } from "../../contexts/userContext";
+import GoogleLogin from "react-google-login";
 
 export default function LoginPage() {
   const [errors, setErrors] = useState([]);
@@ -29,6 +34,11 @@ export default function LoginPage() {
       handleErrors(ex, setErrors);
       console.log(ex);
     }
+  };
+
+  const handleGoogleLogin = async (googleData) => {
+    console.log("Success");
+    console.log(googleData);
   };
 
   const handleEmailChange = (newValue) => {
@@ -71,6 +81,18 @@ export default function LoginPage() {
           <button className="signInButton" onClick={() => handleLoginClick()}>
             Sign in
           </button>
+          <form
+            method="GET"
+            action={"https://localhost:4004/api/useraccount/googlelogin"}
+          >
+            <a onClick={googleLogin}>Google Login</a>
+            <button type="submit">Google Login</button>
+          </form>
+          <GoogleLogin
+            clientId="715533597070-a5oamiocsjaheqnvirc55g1j14avef47.apps.googleusercontent.com"
+            buttonText="Log in with Google"
+            onSuccess={handleGoogleLogin}
+          />
         </div>
       </div>
     </div>
--- src/services/userAccountService.js
+++ src/services/userAccountService.js
@@ -71,3 +71,34 @@ export async function updateAccount() {
 }
 
 // edw pera save the jwt and so on...
+
+export async function googleLogin()
+{
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    }
+    let googleLogin = apiEndpoint + "googleLogin";
+    // const result = await http.get(googleLogin);
+    let xhr = new XMLHttpRequest();
+    var json_obj, status = false;
+    xhr.open("GET",googleLogin,true);
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            var json_obj = JSON.parse(xhr.responseText);
+            status = true;
+            this.setState({ json_obj });
+          } else {
+            console.error(xhr.statusText);
+          }
+        }
+      }.bind(this);
+      xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+      };
+      xhr.send(null);
+    
+    //return result;
+}
